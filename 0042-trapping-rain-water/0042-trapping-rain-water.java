fe9a48ac09d6287ac52ebc066b177d98c4b131f8
NigelTanYong <nigel.ty28@gmail.com>
2025-05-04 21:57:39 +0800

    Time: 0 ms (100%), Space: 46.8 MB (13.97%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,29 @@
+//two-pointer approach
+class Solution {
+    public int trap(int[] height) {
+        int right = height.length -1;
+        int left =0, maxLeft = 0, maxRight =0, totalWaterCollected = 0;
+
+        while(left<right){
+            if(height[left] < height[right]){ //process left side
+                if(height[left] >= maxLeft){ // if previous max height is higher than current
+                    maxLeft = height[left];//set as maxLeft
+                }
+                else{
+                    totalWaterCollected += maxLeft - height[left];
+                }  
+                left++;
+            }
+            else{ //process right side
+                if(height[right] >= maxRight){
+                    maxRight = height[right];
+                }
+                else{
+                    totalWaterCollected += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return totalWaterCollected;
+    }
+}
