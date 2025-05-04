ce5a8d75524a4f9dc1c216fe13231c612e650bfd
NigelTanYong <nigel.ty28@gmail.com>
2025-05-04 11:10:21 +0800

    Time: 2488 ms (5.01%), Space: 46.8 MB (5.25%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> list = new ArrayList<>();
-        int eachWordLen = words[0].length();
-        int totalLen = eachWordLen * words.length;
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String str : words) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
+        if(s.equals("") || words.length==0)
+            return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        int wordCount = words.length;
+        int charPerWordNum = words[0].length();
+        int concatStringLen = wordCount * charPerWordNum;
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+        for(String word : words){
+            wordsMap.put(word, wordsMap.getOrDefault(word,0) + 1);
         }
-
-        for (int i = 0; i <= s.length() - totalLen; i = i + 1) {
-            HashMap<String, Integer> chackMap = new HashMap<>();
-            for (int j = i; j < i + totalLen; j = j + eachWordLen) {
-                String sr = s.substring(j, j + eachWordLen);
-                chackMap.put(sr, chackMap.getOrDefault(sr, 0) + 1);
+        for(int i=0; i<=s.length()-concatStringLen; i++){
+            HashMap<String,Integer> concatMap = new HashMap<>();
+            for(int j =i; j<i+concatStringLen; j+=charPerWordNum){
+                String subString = s.substring(j, j+charPerWordNum);
+                concatMap.put(subString, concatMap.getOrDefault(subString,0) + 1);
             }
-            if (map.equals(chackMap)) {
-                list.add(i);
+            if(wordsMap.equals(concatMap)){
+                result.add(i);
             }
         }
-        return list;
+        return result;
     }
 }
