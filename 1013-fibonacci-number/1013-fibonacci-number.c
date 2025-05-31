0f75cd7624079c414d2b18f19407c050df7cf5dd
NigelTanYong <nigel.ty28@gmail.com>
2025-05-31 09:59:40 +0800

    Time: 3 ms (56.75%), Space: 7.7 MB (46.58%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,15 +1,8 @@
-
+//O(n^2)
 
 int fib(int n){
-     int a = 0, b = 1; //a = F(n-2), b = F(n-1)
-
-    if(n<=1){ // for F(0) and F(1)
+     if(n<=1){
         return n;
-    }
-    for(int i = 2; i<= n; i++){
-        int temp = a+b;
-        a = b;
-        b = temp;
-    }
-    return b;
+     }
+     return (fib(n-1) + fib(n-2));
 }
