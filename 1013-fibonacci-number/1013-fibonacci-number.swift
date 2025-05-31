7dd7f97481ec25c2a7e902321f7eef4fe9f94df9
NigelTanYong <nigel.ty28@gmail.com>
2025-05-31 10:06:33 +0800

    Time: 2 ms (92.25%), Space: 16.8 MB (87.43%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.swift
+++ 1013-fibonacci-number/1013-fibonacci-number.swift
@@ -1,8 +1,16 @@
 class Solution {
     func fib(_ n: Int) -> Int {
-        if n <= 1 {
+        var a = 0
+        var b = 1
+
+        if n <= 1{
             return n
         }
-        return (fib(n-1) + fib(n-2))
+        for i in 2...n {
+            var temp = a + b
+            a = b
+            b = temp
+        }
+        return b
     }
 }
