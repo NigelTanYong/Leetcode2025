ea72488f8acd027810d7bd24265a6cb436b98d08
NigelTanYong <nigel.ty28@gmail.com>
2025-06-04 22:31:57 +0800

    Time: 7 ms (97.94%), Space: 46.6 MB (55.14%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,23 @@
+// O(nlogn)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1)
+            return intervals;
+
+        // sort ascending 
+        Arrays.sort(intervals, (firstInterval, secondInterval) -> Integer.compare(firstInterval[0], secondInterval[0]));
+
+        List<int[]> result = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+        for(int[] interval: intervals){
+            if(interval[0] <= newInterval[1]) //overlap
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            else{
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        } 
+        return result.toArray(new int[result.size()][]);   
+    }
+}
