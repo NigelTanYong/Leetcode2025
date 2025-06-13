0cf1db25c6e3e4bf0f3e13451071c0c225f7b550
NigelTanYong <nigel.ty28@gmail.com>
2025-06-13 23:23:40 +0800

    Time: 0 ms (100%), Space: 42.6 MB (11.79%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+//binary search approach -- keeps dividing in half and check low and high side till answer is reached on mid
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while(low<=high){
+            int mid = (low + high) / 2;
+
+            if(nums[mid] == target) return mid;
+            if(nums[low] <= nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high = mid - 1; //bring high to lower half
+                }
+                else{ // nums[low] > target || target>=nums[mid]
+                    low = mid + 1; // bring low to high half
+                }
+            }
+            else{ // nums[low] > nums[mid]
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
+        } 
+        return -1;
+
+    }
+}
