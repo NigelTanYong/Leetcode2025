857fc6dacacf346298618105d0f71e568ba43de7
NigelTanYong <nigel.ty28@gmail.com>
2025-06-14 22:08:30 +0800

    Time: 2 ms (99.84%), Space: 49.8 MB (45.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -8,20 +8,22 @@ class Solution {
         m = grid[0].length;
         for(int i = 0; i<n; i++){
             for(int j =0; j<m; j++){
-                if(grid[i][j] == '1'){
-                    DFS(grid, i,j);
-                    ++count;
+                if(grid[i][j] == '1'){ //If the cell is land ('1'), we found a new island
+                    DFS(grid, i,j); //mark whole island
+                    ++count; // counts island
                 }
             }
         }
         return count;
     }
     private void DFS(char[][] grid, int i, int j){
+        // Base case: if out of bounds or at water ('0'), stop recursion
         if(i<0 || j<0||i>=n||j>=m||grid[i][j] != '1') return;
         grid[i][j] = '0';
-        DFS(grid, i+1, j);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
-        DFS(grid, i,j-1);
+        // Recursively visit all 4 adjacent directions (up, down, left, right)
+        DFS(grid, i+1, j); //down
+        DFS(grid, i-1, j); //up
+        DFS(grid, i, j+1); //right
+        DFS(grid, i,j-1); // left
     }
 }
