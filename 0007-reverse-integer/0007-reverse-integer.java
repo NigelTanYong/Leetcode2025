a4b51c8599d6da02059b35149188572ba607bb1b
NigelTanYong <nigel.ty28@gmail.com>
2025-06-14 15:14:50 +0800

    Time: 1 ms (83.19%), Space: 40.8 MB (72.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,13 +3,12 @@
 class Solution {
     public int reverse(int x) {
         int result = 0;
-        while(x != 0){//overflow check; /10 is to compensate the *10 later
-            if(result > Integer.MAX_VALUE/10) return 0;
-            if(result < Integer.MIN_VALUE/10) return 0;
 
-            int remainder = x%10;
-            result = result * 10 + remainder; // result * 10 is to multiply the previous iteration remainder
-            x = x/10;
+        while(x!=0){
+            if((result < Integer.MIN_VALUE/10) || (result > Integer.MAX_VALUE/10)) return 0; // /10 becuase of the *10 in result
+            int remainder =  x%10;
+            result = result * 10 + remainder; // mutiply 10 for each digit
+            x/=10;
         }
         return result;
     }
