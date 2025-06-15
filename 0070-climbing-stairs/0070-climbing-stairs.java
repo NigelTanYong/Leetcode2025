aae01634edfae89882aa3c1d377645d71724302a
NigelTanYong <nigel.ty28@gmail.com>
2025-06-15 19:20:44 +0800

    Time: 0 ms (100%), Space: 41.1 MB (5.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,4 @@
-//similar to itreative fibonacci approach
+//similar to itreative fibonacci approach DP
 //ways(n) = ways(n - 1) + ways(n - 2)
 //Time Complexity: O(n), Space Complexity: O(1)
 class Solution {
@@ -6,7 +6,7 @@ class Solution {
         if(n ==1){ // only one way if n=1
             return 1;
         }
-        int a = 1, b = 2; // base case where n=2
+        int a = 1, b = 2; // base e.g.  ways(n-2), ways(n-1)
 
         for(int i = 3; i<=n; i++){ // for n=3 onwards
             int temp = a+b;
