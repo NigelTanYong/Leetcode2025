7697d34a4f140730b607a869de889e6acb57f716
NigelTanYong <nigel.ty28@gmail.com>
2025-06-15 19:16:37 +0800

    Time: 0 ms (100%), Space: 40.2 MB (91.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,14 @@
-//itreative fibonacci approach
-
+//similar to itreative fibonacci approach
+//ways(n) = ways(n - 1) + ways(n - 2)
+//Time Complexity: O(n), Space Complexity: O(1)
 class Solution {
     public int climbStairs(int n) {
-        if(n ==1){ // only one way if 1
+        if(n ==1){ // only one way if n=1
             return 1;
         }
-        int a = 1, b = 2; // 
+        int a = 1, b = 2; // base case where n=2
 
-        for(int i = 3; i<=n; i++){
+        for(int i = 3; i<=n; i++){ // for n=3 onwards
             int temp = a+b;
             a = b;
             b = temp;
