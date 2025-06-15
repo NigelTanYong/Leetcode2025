bb767f61898920a116e9fcf02cb8e988c57661b9
NigelTanYong <nigel.ty28@gmail.com>
2025-06-15 19:06:15 +0800

    Time: 0 ms (100%), Space: 40.4 MB (65.74%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+//itreative fibonacci approach
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n ==1){ // only one way if 1
+            return 1;
+        }
+        int a = 1, b = 2; // 
+
+        for(int i = 3; i<=n; i++){
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+
+    }
+}
