aed99e2bc5f2ff98bab8d12acbe26008db6f62a8
NigelTanYong <nigel.ty28@gmail.com>
2025-06-15 16:32:21 +0800

    Time: 26 ms (39.88%), Space: 58.2 MB (40.18%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -4,20 +4,19 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         int sum = 0;
-        Map<Integer, Integer> remainderMap = new HashMap<>(); //key: remainder of sum/k, value: index
-        remainderMap.put(0,-1); //handle where subarray starts from index 0 -- for the if condition in loop later 
-        
-        for(int i=0; i<nums.length; i++){
-            sum += nums[i];
-            int remainder = sum%k;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
 
-            if(remainderMap.containsKey(remainder)){ //means sum is multiple
-                if(i - remainderMap.get(remainder) >=2 ){ // make sure sub array is 2 or more
+        for(int i = 0; i<nums.length; i++){
+            sum+=nums[i];
+            int remainder = sum%k;
+            if(map.containsKey(remainder)){
+                if(i-map.get(remainder)>=2){
                     return true;
                 }
             }
             else{
-                remainderMap.put(remainder, i);
+                map.put(remainder, i);
             }
         }
         return false;
