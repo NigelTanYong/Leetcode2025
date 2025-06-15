906823b335ec1d539f725b6396f9c84aa373e6e1
NigelTanYong <nigel.ty28@gmail.com>
2025-06-15 15:32:12 +0800

    Time: 14 ms (31.3%), Space: 45.1 MB (28.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,10 @@
-//2 pointer approach, O(n)
+//2 pointer approach, time complexity:O(n) , space complexity: O(n)
 class Solution {
     public boolean isPalindrome(String s) {
         String lowerCaseAlphaNumOnly = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         int left = 0, right = lowerCaseAlphaNumOnly.length()-1;
 
-        while(left<right){
+        while(left<right){ //compare till the mid
             if(lowerCaseAlphaNumOnly.charAt(left) != lowerCaseAlphaNumOnly.charAt(right)){
                 return false;
             }
