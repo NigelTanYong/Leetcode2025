6748f4b07a870d5b32b00637b41b9039839bec1a
NigelTanYong <nigel.ty28@gmail.com>
2025-06-18 22:32:43 +0800

    Time: 23 ms (33.5%), Space: 60.6 MB (61.65%) - LeetHub

--- 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.java
+++ 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.java
@@ -1,12 +1,24 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
+        //sort from small to large
         Arrays.sort(batteries);
         long sum = 0;
-        for (int a : batteries)
-            sum += a;
-        int k = 0, na = batteries.length;
-        while (batteries[na - 1 - k] > sum / (n - k))
-            sum -= batteries[na - 1 - k++];
-        return sum / (n - k);
+
+        for(int minute : batteries){ //to get total minute available form batteries
+            sum+=minute;
+        }
+
+        int i = 0, numOfBatteries = batteries.length;
+        while(batteries[(numOfBatteries - 1) - i] > sum/(n - i)){ // suppose giving each of remaining computer equal time (sum/numOfBatteries - i)
+            /*If a battery is larger than this fair share,
+            then it’s too big to include in the fair division (since it would force others to have less).
+            So we remove that battery and treat it as a dedicated battery, not part of the shared pool*/
+            sum -= batteries[(numOfBatteries - 1) - i];
+            i++;
+        }
+        //After removing overpowered batteries, 
+        //divide remaining total power equally among the remaining n - k computers
+        return sum/(n-i);
+
     }
 }
