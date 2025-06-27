dc650074911b19f8f788d6674fb723da7550167c
NigelTanYong <nigel.ty28@gmail.com>
2025-06-27 21:40:14 +0800

    Time: 40 ms (76.63%), Space: 53.9 MB (80%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -6,6 +6,7 @@
  */
 var reduce = function(nums, fn, init) {
     let result = init;
+    if(nums.length === 0) return init;
     for(let i=0; i<nums.length; i++){
         result = fn(result, nums[i]);
     }
