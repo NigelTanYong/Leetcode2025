26cb3334e19ddfedb4a7e6d484d971e1538f4b09
NigelTanYong <nigel.ty28@gmail.com>
2025-07-15 22:41:32 +0800

    Time: 4 ms (95.87%), Space: 44.9 MB (21.09%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int verticalDeviation = 0, horizontalDeviation = 0;
+
+        for(char c : moves.toCharArray()){
+            if(c == 'R'){
+                horizontalDeviation++;
+            }
+            else if(c == 'L'){
+                horizontalDeviation--;
+            }
+            else if(c == 'U'){
+                verticalDeviation++;
+            }
+            else{
+                verticalDeviation--;
+            }
+        }
+        return (verticalDeviation == 0 && horizontalDeviation == 0) ? true : false;
+    }
+}
