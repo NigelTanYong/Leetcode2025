36285efb38a2efc60a16a3ec265cec6ec651e6e5
NigelTanYong <nigel.ty28@gmail.com>
2025-07-26 16:46:58 +0800

    Time: 2 ms (85.86%), Space: 48 MB (82.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -11,32 +11,39 @@
  //Time Complexity: O(n), Space Complexity: O(1)
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null||head.next==null) return;
-        //find middle of list
-        ListNode slow=head, fast=head;
-        while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        //notice that we can reverse second half then merge to reorder this list
+        //1: find the middle of list
+        //2: reverse the second half
+        //3: merge
+
+        if(head==null || head.next==null) return;
+        //1 - find middle of list
+        ListNode slowNode = head, fastNode = head;
+        while(fastNode!=null && fastNode.next!=null){
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next; //traverse 2 times faster
         }
-        //revers second half
-        ListNode secHalf=slow.next;
-        slow.next=null;
-        ListNode prev=null;
-        while (secHalf!=null){
-            ListNode temp = secHalf.next; //store next pointer
-            secHalf.next = prev; //reverse pointer
-            prev=secHalf; // move prev forward
-            secHalf=temp; // move secHalf forward
+        //2 - reverse second half
+        ListNode secHalf = slowNode.next;
+        slowNode.next = null; // prevent cycle
+        ListNode prev = null;
+        while(secHalf!=null){
+            ListNode temp = secHalf.next;
+            secHalf.next = prev;
+            prev=secHalf;
+            secHalf=temp;
         }
-        //merge two halves
+        //3 - merge
         ListNode firstHalf = head;
+        //at this point prev pointing to the  1st of sechalf
         while(prev!=null){
             ListNode temp1 = firstHalf.next;
             ListNode temp2 = prev.next;
             firstHalf.next = prev;
-            prev.next = temp1;
-            firstHalf = temp1;
-            prev=temp2;
+            prev.next=temp1;//point prev to the next firstHalf
+            firstHalf=temp1; //forward firstHalf pointer
+            prev = temp2; //forward prev
         }
+        
     }
 }
