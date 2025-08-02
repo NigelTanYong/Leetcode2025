344bc7455ddd923301d6c676364ab9e220360148
NigelTanYong <nigel.ty28@gmail.com>
2025-08-02 22:12:05 +0800

    Time: 0 ms (100%), Space: 45 MB (13.07%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -0,0 +1,22 @@
+//do it in-place
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        //let n be no. of rows
+        //use 2 pointers to do flipping and inverting with xor
+        int rows = image.length;
+        for(int i =0; i<rows; i++){
+            int left=0, right = image[i].length -1;
+            while(left<=right){
+                //invert
+                int tempLeft = image[i][left] ^ 1;
+                int tempRight = image[i][right] ^ 1;
+                //Swap with inverted result
+                image[i][left] = tempRight;
+                image[i][right] = tempLeft;
+                left++;
+                right--;
+            }
+        }
+        return image;
+    }
+}
