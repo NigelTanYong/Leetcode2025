1cd17116c1a52679e3a0ed93fc2c03c45427c9a8
NigelTanYong <nigel.ty28@gmail.com>
2025-08-02 22:54:59 +0800

    Time: 0 ms (100%), Space: 42 MB (46.97%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,36 @@
+//Space complexity: O(1)
+//time Complexity: O(n)
 class Solution {
     public void sortColors(int[] nums) {
-        int t = 0;
-        int redCount = 0, whiteCount = 0, blueCount =0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] == 0){
+        //index to follow through final array
+        //use counter to count freqency of each colour
+        //set colours accordingly
+        int index=0;
+        int redCount=0, whiteCount=0, blueCount=0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0)
                 redCount++;
-            }
-            else if(nums[i] == 1){
+            else if(nums[i]==1)
                 whiteCount++;
-            }
-            else{
+            else
                 blueCount++;
-            }
-        }
-        while(redCount != 0){
-            nums[t] = 0;
-            t++;
-            redCount--;
-        }
-        while(whiteCount !=0){
-            nums[t] = 1;
-            t++;
-            whiteCount--;
-        }
-        while(blueCount !=0){
-            nums[t] = 2;
-            t++;
-            blueCount--;
         }
 
+        while(redCount!=0){
+            nums[index]=0;
+            redCount--;
+            index++;
+        }
+        while(whiteCount!=0){
+            nums[index]=1;
+            whiteCount--;
+            index++;
+        }
+        while(blueCount!=0){
+            nums[index]=2;
+            blueCount--;
+            index++;
+        }
     }
 }
