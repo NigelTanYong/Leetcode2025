b4fb521032c4440e772fb55076b29a0f6055a594
NigelTanYong <nigel.ty28@gmail.com>
2025-08-02 17:54:01 +0800

    Time: 0 ms (100%), Space: 42.8 MB (46.41%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -0,0 +1,23 @@
+//Geedy
+//Time complexity: O(n)
+//Space complexity: (O(1))
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        //while till the position is 2nd last becuase end with 0
+        //skip two bits when see 1
+        //see 0, that is one bit
+        //return true if position reach last bits
+        //if postion is more, then false
+
+        int i = 0;
+        while(i<bits.length-1){
+            if(bits[i] == 1){
+                i+=2; //two bit
+            }
+            else{
+                i++; //one bit
+            }
+        }
+        return i==bits.length-1;
+    }
+}
